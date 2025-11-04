77b41e10f2375622419a72eb0ff86b681ac280d9
khoi-nguyen99 <nguyendepzai1105@gmail.com>
2025-11-04 14:45:34 +0700

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,13 @@
+print("Sinh vien: Dinh viet khoi Nguyen")
+print("Ma so SV  :  245752021610082")
+print("################################")
+
+def sum(a, b):
+    # Tính tổng và in kết quả ra màn hình
+    print("sum = " + str(a + b))
+
+# Tính tổng 2 số 4, 5
+sum(4, 5)
+
+# Tính tổng 2 số 3, 7
+sum(3, 7)
---
+++ 3.2.py
@@ -0,0 +1,13 @@
+print("Sinh vien: Dinh viet khoi Nguyen")
+print("Ma so SV  :  245752021610082")
+print("################################")
+
+def sum(a, b):
+    # Trả về kết quả tổng của a và b
+    return a + b
+
+# Gọi hàm và gán kết quả trả về vào biến 'c'
+c = sum(4, 5)
+
+# In kết quả đã được lưu trong biến 'c'
+print("Tong cua 4 va 5 = " + str(c))
