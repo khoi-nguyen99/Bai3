9942ffa3e1149c69940b883d081781f9098c2eda
khoi-nguyen99 <nguyendepzai1105@gmail.com>
2025-11-04 15:24:50 +0700

    Add files via upload

---
+++ 3.3.py
@@ -0,0 +1,9 @@
+print("Sinh vien: Dinh viet khoi Nguyen")
+print("Ma so SV  :  245752021610082")
+print("################################")
+
+def say_hello():
+ a = "Hello"
+ print(a)
+
+say_hello() # Cần gọi hàm để in ra "Hello"
---
+++ 3.4.py
@@ -0,0 +1,10 @@
+print("Sinh vien: Dinh viet khoi Nguyen")
+print("Ma so SV  :  245752021610082")
+print("################################")
+
+a = "Hello Guy!"
+def say(a):
+ a = "Vinh University"
+ print(a)
+say(a)
+print(a)
